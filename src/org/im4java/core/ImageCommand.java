40319b1cfdc7468cfd32a596a9f8951bc1aa9771
Didac <didac.montero.mendez@gmail.com>
2014-12-08 23:52:18 +0100

    Update ImageCommand.java
    Causes NullPointerException in Windows when using with CompareCmd: https://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=24053

--- src/org/im4java/core/ImageCommand.java
+++ src/org/im4java/core/ImageCommand.java
@@ -249,7 +249,7 @@ public class ImageCommand extends ProcessStarter implements ErrorConsumer {
   protected void finished(int pReturnCode) throws Exception {
     if (pReturnCode > 0) {
       CommandException ce;
-      if (iErrorText.size() > 0) {
+      if (iErrorText != null && iErrorText.size() > 0) {
 	ce = new CommandException(iErrorText.get(0));
       } else {
 	ce = new CommandException("return code: " + pReturnCode);
